f718df7e3d5321a2ee10bd1ac4184afd362700c6
Gillian Gracey <g.gracey@claritypartners.com>
2023-04-10 11:30:43 -0500

    adding python file

---
+++ awesomeness.py
@@ -0,0 +1,15 @@
+import json
+#rev = open('review_training.json')
+scores = open('product_training.json')
+
+def varmean():
+    for i in scores['awesomeness']:
+        print(i)
+    return
+
+
+varmean()
+
+
+def cor_coeff(variable, awesomeness):
+    return 0
